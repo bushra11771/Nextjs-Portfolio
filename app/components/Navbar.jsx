80dd77ae588fc25ef1d3ae76724b64e10252420c
bushra11771 <bushra11771@gmail.com>
2023-10-16 20:53:39 +0500

    text update

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
+import Image from "next/image";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
@@ -26,12 +27,17 @@ const Navbar = () => {
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Image
+          src="/images/logo.png"
+          alt="hero image"
+          className="absolute"
+          width={300}
+          height={300}
+        />
         <Link
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
-        >
-          Bushra
-        </Link>
+        ></Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
             <button
